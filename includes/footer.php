d0d12a122374577491389865a2dc7b0cb1a4d5c8
Hideki69 <kervan.mazuy@gmail.com>
2017-08-23 15:43:36 +0200

    Creation Site Présentation ONLGSP
    - Création de la structure du site (css, js, font, includes, meta)
    - Mise en Place de Jquery / Bootstrap

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+</main>
+<footer>
+
+
+
+</footer>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include'includes/header.php' ?>
+<?php include'includes/news.php' ?>
+<?php include'includes/listeJeux.php' ?>
+<?php include'includes/forfait.php' ?>
+<?php include'includes/imgPanel.php' ?>
+<?php include'includes/footer.php' ?>
