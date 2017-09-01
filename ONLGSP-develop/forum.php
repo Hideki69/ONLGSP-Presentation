d3b5a95f5d8803f6e3b910eea6f6bb1d8f4c9d9a
Hideki69 <kervan.mazuy@gmail.com>
2017-09-01 08:46:43 +0200

    design Forum
    - css sur forum

--- ONLGSP-develop/forum.php
+++ ONLGSP-develop/forum.php
@@ -5,17 +5,19 @@ include'includes/menu.php';
 include'includes/connexion.php';
 
 ?>
-<h1 class="text-center titreForum">Section discution</h1>
-<div class="row text-center">
-    <a href="sectionJeu.php"> Section Jeux /</a>
-    <a href="index.php"> Accueil </a>
-</div>
-<div class="row">
-    <div class="col-md-offset-2 col-md-8">
-        <div class="bordure"></div>
+<div class="col-md-12 bordureTitreJeux">
+    <h1 class="text-center titreForum">Section discution</h1>
+    <div class="row text-center">
+        <a href="sectionJeu.php"> Section Jeux /</a>
+        <a href="index.php"> Accueil </a>
     </div>
+    <div class="row">
+        <div class="col-md-offset-2 col-md-8">
+            <div class="bordure"></div>
+        </div>
+    </div>
+    <img src="images/Logo.png" class="col-md-offset-4 col-md-4 img-responsive" alt="Logo ONLGSP">
 </div>
-
 <?php
 // On récupère les 5 derniers billets
 $req = $dbh->query('SELECT id_billet, titre, contenu, auteur, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
@@ -33,7 +35,7 @@ while ($donnees = $req->fetch())
             <!-- On cells (`td` or `th`) -->
             <tbody>
                 <td class="bordureDroite col-md-4"><p class="text-center sujet">Auteur</p><h3 class="text-center"><br><strong><?php echo htmlspecialchars($donnees['auteur']); ?></strong></h3></td>
-                <td class="bordureDroite col-md-4">
+                <td class="col-md-4">
                     <p class="sujet text-center"> Sujet </p>
                     <h4 class="text-center">
                         <?php echo htmlspecialchars($donnees['titre']); ?>
@@ -41,7 +43,7 @@ while ($donnees = $req->fetch())
                     </h4>
                     <a href="commentaire.php?billet=<?php echo $donnees['id_billet']; ?>"><h4>Commentaires</h4></a>
                 </td>
-                <td class="col-md-4">
+                <td class="bordureDroite col-md-4">
                     <p class="sujet text-center"> Accroche </p>
                     <p class="text-center">
                         <?php
--- ONLGSP-develop/sectionJeu.php
+++ ONLGSP-develop/sectionJeu.php
@@ -3,18 +3,20 @@ include'includes/entete.php';
 include'includes/menu.php';
 include'includes/connexion.php';
 ?>
-<h1 class="text-center titreForum">Section Jeux</h1>
 
-<div class="row text-center">
-    <a href="forum.php"> Section Discution /</a>
-    <a href="index.php"> Accueil </a>
-</div>
-<div class="row">
-    <div class="col-md-offset-2 col-md-8">
-        <div class="bordure"></div>
+<div class="col-md-12 bordureTitreJeux">
+    <h1 class="text-center titreForum">Section Jeux</h1>
+    <div class="row text-center">
+        <a href="forum.php"> Section Discution</a><span>  /  </span>
+        <a href="index.php"> Accueil </a>
     </div>
+    <div class="row">
+        <div class="col-md-offset-2 col-md-8">
+            <div class="bordure"></div>
+        </div>
+    </div>
+    <img src="images/Logo.png" class="col-md-offset-4 col-md-4 img-responsive" alt="Logo ONLGSP">
 </div>
-
 <?php
 // On récupère les 5 derniers billets
 $req = $dbh->query('SELECT id_billetJeux, titre, contenuBilletJeux, auteur, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billetJeux ORDER BY titre ASC');
@@ -31,17 +33,17 @@ while ($donnees = $req->fetch())
         <table class="table table-striped">
             <!-- On cells (`td` or `th`) -->
             <tbody>
-                <td class="bordureDroite col-md-6"><p class="text-center sujet">Auteur</p><h3 class="text-center"><br><strong><?php echo htmlspecialchars($donnees['auteur']); ?></strong></h3></td>
+                <td class="bordureDroite col-md-6"><p class="text-center col-md-offset-2 col-md-8 sujet">Auteur</p><h3 class="text-center"><br><strong><?php echo htmlspecialchars($donnees['auteur']); ?></strong></h3></td>
                 <td class="col-md-6">
-                    <p class="sujet text-center"> Jeux </p>
+                    <p class="col-md-offset-2 col-md-8 sujet text-center"> Jeux </p><br>
                     <h4 class="text-center">
-                        <?php echo strtoupper(htmlspecialchars($donnees['titre'])); ?>
+                        <strong><?php echo strtoupper(htmlspecialchars($donnees['titre'])); ?></strong>
                         <em>le <?php echo $donnees['date_creation_fr']; ?></em>
                     </h4>
-                    
+
                     <p>
                         <?php
-                        echo nl2br(htmlspecialchars($donnees['contenuBilletJeux']));
+    echo nl2br(htmlspecialchars($donnees['contenuBilletJeux']));
                         ?>
                     </p>
                     <a href="commentaireJeux.php?billet=<?php echo $donnees['id_billetJeux']; ?>"><h4>Commentaires</h4></a>
